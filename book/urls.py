fd0e18ec56b57dd3f19bef8bd1ea33ddc56a35b8
sumiok <tap.miwo@gmail.com>
2023-02-25 11:47:38 +0900

    control

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
 from django.urls import path
-
+from .views import sample
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("sample",sample,name='sample'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import SampleModel
 # Create your views here.
+def sample(request):
+    object_lsit = SampleModel.objects.all()
+    return render(request,'sample.html',{'objects': object_lsit})
