26c0a1bf3683d39eb8d5bd095e74328f3ce9be1e
sumiok <tap.miwo@gmail.com>
2023-02-27 19:01:58 +0900

    create

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import sample, Listview, DetailBookView
+from .views import sample, Listview, DetailBookView, CreateBookView
 
 urlpatterns = [
     path("sample",sample,name='sample'),
-    path("book/", Listview.as_view()),
-    path("book/detail/<int:pk>", DetailBookView.as_view()),
+    path("book/", Listview.as_view(), name='list-book'),
+    path("book/detail/<int:pk>", DetailBookView.as_view(),name='detail'),
+    path("book/create/", CreateBookView.as_view(), name='create')
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from .models import SampleModel, Book
 # Create your views here.
 def sample(request):
@@ -13,3 +14,9 @@ class Listview(ListView):
 class DetailBookView(DetailView):
     template_name = 'book/book_detail.html'
     model = Book
+
+class CreateBookView(CreateView):
+    template_name = 'book/book_create.html'
+    model = Book
+    fields = ('title', 'text', 'category')
+    success_url = reverse_lazy('list-book')
